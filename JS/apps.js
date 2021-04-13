227aef2453e91cbe2ebbdf114929b2ccae543cdb
Tina Shabestari <tshabestari@my.bcit.ca>
2021-04-12 22:36:05 -0700

    Add and included the budget function between two numbers and find the expenses for the numbers which is created for.

---
+++ JS/apps.js
@@ -0,0 +1,20 @@
+// Include for the first budget input.
+function CreateBudget() 
+{
+    budget = document.getElementById("budget").value;
+    document.getElementById("result-one").innerHTML = budget;
+}
+// Putting the 
+function AddBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 + num2;
+}
+
+function SubtractBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = value - budget;   
+}
